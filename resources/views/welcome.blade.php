bcb3638b6af665b7044a25f697e65f5e1341ffb7
RIRIRNJFVNU <91568638+RIRIRNJFVNU@users.noreply.github.com>
2022-01-13 02:04:21 +0100

    versionfinal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,23 +5,6 @@
     @push('welcome')
         <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
     @endpush
-
-    {{-- @if (Route::has('login'))
-        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            @auth
-                <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-            @else
-                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}"
-                        class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                @endif
-            @endauth
-        </div>
-    @endif --}}
-
-    {{-- Welcome Page d'Elise --}}
     <div class="welcome-container">
         <section class="welcome-one">
             <div class="welcome-item">
@@ -31,8 +14,6 @@
                     <br />
                     Rejoignez-nous maintenant
                 </h2>
-                {{-- <button type="button" class="btn btn-primary">Créer un compte</button>
-                <button type="button" class="btn btn-primary">Se connecter</button> --}}
                 <a href="{{ route('login') }}" role="button" class="btn btn-primary btn-redirect-auth">
                     Connectez-Vous
                 </a>
